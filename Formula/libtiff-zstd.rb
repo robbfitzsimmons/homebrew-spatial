ed08f3fd25af37f95e88fa2f1ac28fbe4df069c8
Robb Fitzsimmons <robb.fitzsimmons@linnaean.co>
2022-05-28 19:37:20 -0500

    libtiff-zstd 4.4.0 (new formula)
    * libtiff 4.3.0 with zstd codec enabled (new formula)
    * libtiff 4.3.0 with zstd codec enabled (fix formula)
    * disable LZMA and WEBP (hoping for ubuntu test pass)
    * declare zstd dependency
    * Declare zlib dependency (Ubuntu?)
    * Update libtiff to 4.4.0 before trying other modifications for Ubuntu build
    * Update libtiff to 4.4.0, fix indentation
    * Update libtiff to 4.4.0, fix naming bug
    * Try linking external libjbig
    * Try linking external libjbig (again?)
    
    Closes #1.
    
    Signed-off-by: Robb Fitzsimmons <robb@linnaean.co>

---
+++ Formula/libtiff-zstd.rb
@@ -0,0 +1,50 @@
+class LibtiffZstd < Formula
+  desc "TIFF library and utilities with ZSTD codec enabled"
+  homepage "https://libtiff.gitlab.io/libtiff/"
+  url "https://download.osgeo.org/libtiff/tiff-4.4.0.tar.gz"
+  mirror "https://fossies.org/linux/misc/tiff-4.4.0.tar.gz"
+  sha256 "917223b37538959aca3b790d2d73aa6e626b688e02dcda272aec24c2f498abed"
+  license "libtiff"
+
+  livecheck do
+    url "https://download.osgeo.org/libtiff/"
+    regex(/href=.*?tiff[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "jbigkit"
+  depends_on "jpeg"
+
+  uses_from_macos "zlib"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --disable-lzma
+      --disable-webp
+      --with-jbig-include-dir=#{Formula["jbigkit"].opt_include}
+      --with-jpeg-include-dir=#{Formula["jpeg"].opt_include}
+      --with-jpeg-lib-dir=#{Formula["jpeg"].opt_lib}
+      --without-x
+    ]
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <tiffio.h>
+
+      int main(int argc, char* argv[])
+      {
+        TIFF *out = TIFFOpen(argv[1], "w");
+        TIFFSetField(out, TIFFTAG_IMAGEWIDTH, (uint32) 10);
+        TIFFClose(out);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-ltiff", "-o", "test"
+    system "./test", "test.tif"
+    assert_match(/ImageWidth.*10/, shell_output("#{bin}/tiffdump test.tif"))
+  end
+end
