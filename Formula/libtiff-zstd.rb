e998f4942fe382ac50b635c098555ee43487d063
Robb Fitzsimmons <robb.fitzsimmons@linnaean.co>
2022-05-30 19:07:12 -0500

    Update libtiff to 4.4.0, fix indentation

--- Formula/libtiff-zstd.rb
+++ Formula/libtiff-zstd.rb
@@ -1,4 +1,4 @@
-class LibtiffZstd < Formula
+class Libtiff < Formula
   desc "TIFF library and utilities with ZSTD codec enabled"
   homepage "https://libtiff.gitlab.io/libtiff/"
   url "https://download.osgeo.org/libtiff/tiff-4.4.0.tar.gz"
@@ -16,30 +16,33 @@ class LibtiffZstd < Formula
   uses_from_macos "zlib"
 
   def install
-      args = %W[
-        --prefix=#{prefix}
-        --disable-dependency-tracking
-        --with-jpeg-include-dir=#{Formula["jpeg"].opt_include}
-        --with-jpeg-lib-dir=#{Formula["jpeg"].opt_lib}
-        --without-x
-      ]
-      system "./configure", *args
-      system "make", "install"
-    end
-
-    test do
-      (testpath/"test.c").write <<~EOS
-        #include <tiffio.h>
-        int main(int argc, char* argv[])
-        {
-          TIFF *out = TIFFOpen(argv[1], "w");
-          TIFFSetField(out, TIFFTAG_IMAGEWIDTH, (uint32) 10);
-          TIFFClose(out);
-          return 0;
-        }
-      EOS
-      system ENV.cc, "test.c", "-L#{lib}", "-ltiff", "-o", "test"
-      system "./test", "test.tif"
-      assert_match(/ImageWidth.*10/, shell_output("#{bin}/tiffdump test.tif"))
-    end
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --disable-lzma
+      --disable-webp
+      --with-jpeg-include-dir=#{Formula["jpeg"].opt_include}
+      --with-jpeg-lib-dir=#{Formula["jpeg"].opt_lib}
+      --without-x
+    ]
+    system "./configure", *args
+    system "make", "install"
   end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <tiffio.h>
+
+      int main(int argc, char* argv[])
+      {
+        TIFF *out = TIFFOpen(argv[1], "w");
+        TIFFSetField(out, TIFFTAG_IMAGEWIDTH, (uint32) 10);
+        TIFFClose(out);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-ltiff", "-o", "test"
+    system "./test", "test.tif"
+    assert_match(/ImageWidth.*10/, shell_output("#{bin}/tiffdump test.tif"))
+  end
+end
