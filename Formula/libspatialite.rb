1be8449efb3a823aa10cc590a4a1e6711d7c2e95
Robb Fitzsimmons <robb@linnaean.co>
2022-08-08 14:51:33 -0500

    libspatialite with zstd support

---
+++ Formula/libspatialite.rb
@@ -0,0 +1,74 @@
+class Libspatialite < Formula
+  desc "Adds spatial SQL capabilities to SQLite"
+  homepage "https://www.gaia-gis.it/fossil/libspatialite/index"
+  license any_of: ["MPL-1.1", "GPL-2.0-or-later", "LGPL-2.1-or-later"]
+
+  stable do
+    url "https://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-5.0.1.tar.gz"
+    mirror "https://ftp.netbsd.org/pub/pkgsrc/distfiles/libspatialite-5.0.1.tar.gz"
+    mirror "https://www.mirrorservice.org/sites/ftp.netbsd.org/pub/pkgsrc/distfiles/libspatialite-5.0.1.tar.gz"
+    sha256 "eecbc94311c78012d059ebc0fae86ea5ef6eecb13303e6e82b3753c1b3409e98"
+
+    # Fix -flat_namespace being used on Big Sur and later.
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
+      sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
+    end
+  end
+
+  livecheck do
+    url "https://www.gaia-gis.it/gaia-sins/libspatialite-sources/"
+    regex(/href=.*?libspatialite[._-]v?(\d+(?:\.\d+)+[a-z]?)\.t/i)
+  end
+
+  head do
+    url "https://www.gaia-gis.it/fossil/libspatialite", using: :fossil
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "freexl"
+  depends_on "geos"
+  depends_on "librttopo"
+  depends_on "libxml2"
+  depends_on "minizip"
+  depends_on "robbfitzsimmons/spatial/proj"
+  depends_on "sqlite"
+
+  def install
+    system "autoreconf", "-fi" if build.head?
+
+    # New SQLite3 extension won't load via SELECT load_extension("mod_spatialite");
+    # unless named mod_spatialite.dylib (should actually be mod_spatialite.bundle)
+    # See: https://groups.google.com/forum/#!topic/spatialite-users/EqJAB8FYRdI
+    #      needs upstream fixes in both SQLite and libtool
+    inreplace "configure",
+              "shrext_cmds='`test .$module = .yes && echo .so || echo .dylib`'",
+              "shrext_cmds='.dylib'"
+    chmod 0755, "configure"
+
+    # Ensure Homebrew's libsqlite is found before the system version.
+    sqlite = Formula["sqlite"]
+    ENV.append "LDFLAGS", "-L#{sqlite.opt_lib}"
+    ENV.append "CFLAGS", "-I#{sqlite.opt_include}"
+
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --with-sysroot=#{HOMEBREW_PREFIX}
+      --enable-geocallbacks
+      --enable-rttopo=yes
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    # Verify mod_spatialite extension can be loaded using Homebrew's SQLite
+    pipe_output("#{Formula["sqlite"].opt_bin}/sqlite3",
+      "SELECT load_extension('#{opt_lib}/mod_spatialite');")
+  end
+end
