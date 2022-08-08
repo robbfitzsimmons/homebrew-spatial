511525eb1d060e4689f89d23ef1f92ea0df09f0d
Robb Fitzsimmons <robb@linnaean.co>
2022-08-08 14:15:58 -0500

    libgeotiff with zstd support

---
+++ Formula/libgeotiff.rb
@@ -0,0 +1,76 @@
+class Libgeotiff < Formula
+  desc "Library and tools for dealing with GeoTIFF"
+  homepage "https://github.com/OSGeo/libgeotiff"
+  license "MIT"
+
+  stable do
+    url "https://github.com/OSGeo/libgeotiff/releases/download/1.7.1/libgeotiff-1.7.1.tar.gz"
+    sha256 "05ab1347aaa471fc97347d8d4269ff0c00f30fa666d956baba37948ec87e55d6"
+
+    # Fix -flat_namespace being used on Big Sur and later.
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+      sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+    end
+  end
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  head do
+    url "https://github.com/OSGeo/libgeotiff.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "jpeg-turbo"
+  depends_on "robbfitzsimmons/spatial/libtiff"
+  depends_on "robbfitzsimmons/spatial/proj"
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", *std_configure_args, "--with-jpeg"
+    system "make" # Separate steps or install fails
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include "geotiffio.h"
+      #include "xtiffio.h"
+      #include <stdlib.h>
+      #include <string.h>
+
+      int main(int argc, char* argv[])
+      {
+        TIFF *tif = XTIFFOpen(argv[1], "w");
+        GTIF *gtif = GTIFNew(tif);
+        TIFFSetField(tif, TIFFTAG_IMAGEWIDTH, (uint32) 10);
+        GTIFKeySet(gtif, GeogInvFlatteningGeoKey, TYPE_DOUBLE, 1, (double)123.456);
+
+        int i;
+        char buffer[20L];
+
+        memset(buffer,0,(size_t)20L);
+        for (i=0;i<20L;i++){
+          TIFFWriteScanline(tif, buffer, i, 0);
+        }
+
+        GTIFWriteKeys(gtif);
+        GTIFFree(gtif);
+        XTIFFClose(tif);
+        return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lgeotiff",
+                   "-L#{Formula["libtiff"].opt_lib}", "-ltiff", "-o", "test"
+    system "./test", "test.tif"
+    output = shell_output("#{bin}/listgeo test.tif")
+    assert_match(/GeogInvFlatteningGeoKey.*123\.456/, output)
+  end
+end
